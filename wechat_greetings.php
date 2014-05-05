b9417a9e2c273af76b17ecdf8a2686b21b131b91
yuerzx <hansun@1230.me>
2014-05-05 21:34:38 +1000

    updated greeting

---
+++ wechat_greetings.php
@@ -0,0 +1,68 @@
+<?php 
+
+//Company information sections Starts
+
+	$Company_intro = array(
+	"0"=>array(
+	'Title'=>'公司简介',
+	'Description'=>'澳发地产由Maggie Ren于2008年一手创立。在短短五年间，迅速成长为墨尔本最顶尖的地产销售服务公司中的龙头企业',
+	'PicUrl'=>'http://mmbiz.qpic.cn/mmbiz/4HwuOCxJvfu7aj9vWeZibDW3OWdACDlUEtRUCoUqIMvZFhnOnQj3cZoJmt3u2CMRZUHMibpMxGmdiazcJ2wzJUKWg/0',
+	'Url'=>'http://mp.weixin.qq.com/s?__biz=MzA3MzU0NjMxMA==&mid=200349073&idx=1&sn=9bfa510f91e95841977ac3b881b95539#rd'
+	) );
+
+	$Company_news = array(
+	"0"=>array(
+	'Title'=>'公司新闻',
+	'Description'=>'澳发地产的微信公共平台服务系统正式上线，为您提供多元化最优质的服务。',
+	'PicUrl'=>'http://mmbiz.qpic.cn/mmbiz/4HwuOCxJvfu7aj9vWeZibDW3OWdACDlUEtRUCoUqIMvZFhnOnQj3cZoJmt3u2CMRZUHMibpMxGmdiazcJ2wzJUKWg/0',
+	'Url'=>'http://mp.weixin.qq.com/s?__biz=MzA3MzU0NjMxMA==&mid=200349073&idx=1&sn=9bfa510f91e95841977ac3b881b95539#rd'
+	) );
+
+	$Company_team = array(
+	"0"=>array(
+	'Title'=>'公司团队',
+	'Description'=>'澳发地产致力于提供最优秀的服务，最良好的团队精神。',
+	'PicUrl'=>'http://www.alphalynx.com.au/Layouts/OurTeam/ourteam1027.jpg',
+	'Url'=>'http://www.maifang.com.au/projects/alphalynx/?page_id=12'
+	) );
+
+/* Compnay information sections ends */
+/* Compnay business sections starts */
+	$Buy_property = array(
+	"0"=>array(
+	'Title'=>'购置房产',
+	'Description'=>'在澳发，我们能为您提供7*24小时的全程服务，帮您选择到最优质的房产。',
+	'PicUrl'=>'http://maifang-com-au.qiniudn.com/Buying-Spanish-Property-Using-a-Company-Corporate.jpg',
+	'Url'=>'http://www.maifang.com.au/projects/alphalynx/?page_id=20'
+	), 
+	"1"=>array(
+	'Title'=>"公寓项目: \n最新公寓，尽在澳发",
+	'Description'=>'',
+	'PicUrl'=>'http://maifang-com-au.qiniudn.com/Buying-Spanish-Property-Using-a-Company-Corporate.jpg',
+	'Url'=>'http://www.maifang.com.au/projects/alphalynx/?page_id=20'
+	), 
+	"2"=>array(
+	'Title'=>"别墅项目: \n顶级豪宅，无与伦比",
+	'Description'=>'',
+	'PicUrl'=>'http://maifang-com-au.qiniudn.com/Buying-Spanish-Property-Using-a-Company-Corporate.jpg',
+	'Url'=>'http://www.maifang.com.au/projects/alphalynx/?page_id=20'
+	), 
+	"3"=>array(
+	'Title'=>"联排别墅: \n左邻右舍，别有风味",
+	'Description'=>'',
+	'PicUrl'=>'http://maifang-com-au.qiniudn.com/Buying-Spanish-Property-Using-a-Company-Corporate.jpg',
+	'Url'=>'http://www.maifang.com.au/projects/alphalynx/?page_id=20'
+	), 
+	"4"=>array(
+	'Title'=>"不知道如何选择？请直接联系我们的销售团队，会为您提供最优良的服务",
+	'Description'=>'',
+	'PicUrl'=>'',
+	'Url'=>'http://www.maifang.com.au/projects/alphalynx/?page_id=12'
+	), 
+	
+	);
+
+
+/* Compnay business sections endss */
+
+?>
