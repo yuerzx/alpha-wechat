213b17cf8077631d64f482bff80b87c54fafe3bb
Han Sun <badfish1230@gmail.com>
2014-06-10 14:48:55 +1000

    Add colors to footer

--- footer.php
+++ footer.php
@@ -4,15 +4,15 @@
 <!-- Footer -->
 <footer>
     <nav class="bar bar-tab">
-        <a class="tab-item" href="http://app.alphalynx.com.au/team-members/">
+        <a class="tab-item" style="color:#df8986;" href="http://app.alphalynx.com.au/team-members/">
             <span class="icon icon-person"></span>
             <span class="tab-label">销售团队</span>
         </a>
-        <a class="tab-item" href="http://app.alphalynx.com.au/category/apartment/">
+        <a class="tab-item" style="color:#df8986;" href="http://app.alphalynx.com.au/category/apartment/">
             <span class="icon icon-star-filled"></span>
             <span class="tab-label">热门房产</span>
         </a>
-        <a class="tab-item" href="http://app.alphalynx.com.au/social/">
+        <a class="tab-item" style="color:#df8986;" href="http://app.alphalynx.com.au/social/">
             <span class="icon icon-info"></span>
             <span class="tab-label">关注澳发</span>
         </a>
